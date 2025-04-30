c0ff15df3d1e48aa20405a1631f5ab9482e16f00
Khushi Bera <145190251+khushi2758@users.noreply.github.com>
2025-05-01 00:39:14 +0530

    update

--- 12-adv-starting-project/frontend/src/pages/Error.js
+++ 12-adv-starting-project/frontend/src/pages/Error.js
@@ -1,9 +1,26 @@
+import { useRouteError } from "react-router-dom";
+import PageContent from "../components/PageContent";
 function ErrorPage() {
+   const error = useRouteError();
+  let title = "An error occurred!";
+  let message = "Something went wrong!";
+  if (error.status === 500) {
+    message = JSON.parse(error.data).message;
+  } else if (error.status === 404) {
+    title = "Not Found!";
+    message = "Could not find the page you are looking for.";
+  } else if (error.status === 403) {
+    title = "Access Denied!";
+    message = "You are not allowed to access this page.";
+  }
   return (
-    <div>
-      <h1>404</h1>
-      <p>Page not found</p>
-    </div>
+    <PageContent title={title}>
+   
+      <p>{message}</p>
+      <p>
+        Back to <a href="/">Home</a>
+  </p>
+    </PageContent>
   );
 }
 
